ac55edc8e661236b67a176b9db5c221a78d30e6b
Jake Whitebread <86974773+jakewhitebread@users.noreply.github.com>
2021-10-08 16:35:32 -0700

    Update Autonomous.py

--- Autonomous.py
+++ Autonomous.py
@@ -66,7 +66,7 @@ def stopBot():
 
 def test():
     try:
-        PWM.setMotorModel(0,0,1000,0)
+        PWM.setMotorModel(0,0,0,1000)
         time.sleep(2)
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
         PWM.setMotorModel(0,0,0,0)
