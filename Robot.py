8ccf7a862b05ac33a375cd037fee3bb1cac99c69
Jake Whitebread <86974773+jakewhitebread@users.noreply.github.com>
2021-10-13 17:11:11 -0700

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -44,14 +44,16 @@ def moveBackward(distance, speed):
     PWM.setMotorModel(0, 0, 0, 0)
 
 def turnLeft(duration, speed):
-    PWM.setMotorModel(speed, -speed/4, speed, -speed/4)
+    float smallSpeed = speed / 4
+    PWM.setMotorModel(speed, -smallSpeed, speed, -smallSpeed)
     # PWM.setMotorModel(2000, -2000, 2000, -2000)
     time.sleep(duration)
     choiceStack.append(Choice(LEFT, duration, speed))
     PWM.setMotorModel(0, 0, 0, 0)
 
 def turnRight(duration, speed):
-    PWM.setMotorModel(-speed, (int) speed/4.0, -speed, (int) speed/4.0)
+    float smallSpeed = speed / 4
+    PWM.setMotorModel(-speed, smallSpeed, -speed, smallSpeed)
     # PWM.setMotorModel(-2000, 2000, -500, 500)
     time.sleep(duration)
     choiceStack.append(Choice(RIGHT, duration, speed))
