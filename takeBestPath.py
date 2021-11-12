cf723710f665e89afc87148d819247baee6582dc
Jake Whitebread <86974773+jakewhitebread@users.noreply.github.com>
2021-11-12 15:37:48 -0700

    Update takeBestPath.py

--- takeBestPath.py
+++ takeBestPath.py
@@ -57,7 +57,7 @@ def drive(moveHistory):
                         camera.start_preview()
                         sleep(5)
                         camera.capture('C:\Users\phogg\OneDrive\Documents\GCU\CST-415\right.jpg')
-                        camera.stop_preview
+                        camera.stop_preview()
                         # moveForward(.8)
                         # stopBot()
                         # turnRight(1.27)
