151f6c2b7f5104571c0bf90be7f487a1e896fc14
Jake Whitebread <86974773+jakewhitebread@users.noreply.github.com>
2021-11-19 14:43:15 -0700

    Update Autonomous_2.py

--- Autonomous_2.py
+++ Autonomous_2.py
@@ -26,7 +26,7 @@ def drive():
     moveHistory = MoveStack()
     try:
         while True:
-            lookStraight()
+            lookForward()
             forwardDistance = sweepThisView()  
             if forwardDistance > distanceTolerance:
                 moveForward(.1)
